394ce9d1774dcf39ae7344d8b731cd276efc762b
Nasser Amer <nasser_amer09@yahoo.com>
2024-02-20 13:45:03 +0100

    first commit

---
+++ src/test/java/com/example/publictransportation/PublicTransportationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publictransportation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicTransportationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
